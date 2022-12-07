7fe07de3aa4c71ecef7ebc1e0ade370e6182ea26
Jeferson Narvaez <jeferson.narvaez.dev@gmail.com>
2022-12-07 11:34:25 -0500

    Install google maps, add configuration android , ios, and use it

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+// ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qr_reader/models/scan_model.dart';
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,15 +1,41 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     final scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
     return Scaffold(
-      appBar: AppBar(title: const Text('Map')),
-      body: Center(child: Text(scan.value)),
-    );
+        appBar: AppBar(title: const Text('Map')),
+        body: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
   }
 }
